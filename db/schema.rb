6a91faa960d9b3935d6761ad72fbb5c7b2731f77
Artur Gafizov <art05xxx@rambler.ru>
2020-01-08 08:48:22 +0300

    Add contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_070359) do
+ActiveRecord::Schema.define(version: 2020_01_07_132407) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "string"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
