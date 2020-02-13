88aed89e5c273228b12b559cb8031fc705b6f01d
Artur Gafizov <art05xxx@rambler.ru>
2020-02-13 20:22:02 +0300

    Add create contact acceptance tests

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+require 'rails_helper'
+
+feature "Contact Creation" do 
+
+	scenario "allows acees to contacts page" do
+		visit '/contacts'
+
+		expect(page).to have_content 'Contact us'
+	end	
+end	
